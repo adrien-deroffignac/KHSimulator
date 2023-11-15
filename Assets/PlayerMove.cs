a5299c2223dc9ecbea3fb4fd15c1ac7e7004080d
adrien-deroffignac <adrien.de_roffignac@edu.devinci.fr>
2023-11-15 17:31:43 +0100

    mouvement

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,5 +1,6 @@
 using Cinemachine;
 using NaughtyAttributes;
+using PlasticPipe.PlasticProtocol.Messages;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -25,14 +26,48 @@ public class PlayerMove : MonoBehaviour
     public Vector2 JoystickDirection { get; private set; }
     Coroutine MovementRoutine { get; set; }
 
+
+
+
     private void Start()
     {
-        
+        _move.action.performed += StartMove;
+        _move.action.canceled += StopMove;
+    }
+
+    private void StartMove(InputAction.CallbackContext obj)
+    {
+        if (MovementRoutine != null)
+        {
+            StopCoroutine(MovementRoutine);
+        }
+
+        MovementRoutine = StartCoroutine(Move());
+        OnStartMove?.Invoke();
+    }
+
+    private IEnumerator Move()
+    {
+       while(true)
+       {
+            var direction = _move.action.ReadValue<Vector2>();
+            Vector3 movement = new(direction.x, 0, direction.y);
+            transform.Translate(_speed * Time.deltaTime * movement.normalized);
+            yield return new WaitForFixedUpdate();
+       }
+    }
+
+    private void StopMove(InputAction.CallbackContext obj)
+    {
+        if (MovementRoutine != null)
+        {
+            StopCoroutine(MovementRoutine);
+        }
     }
 
     private void OnDestroy()
     {
-        
+        _move.action.performed -= StartMove;
+        _move.action.canceled -= StopMove;
     }
-
 }
